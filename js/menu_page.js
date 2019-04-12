24e8afd5d818bdda95e709419daeae81e1efbf6b
siyayang0420 <siyayang0420@live.com>
2019-04-12 11:14:14 -0700

    first upload

---
+++ js/menu_page.js
@@ -0,0 +1,31 @@
+        function showSelectedA() {
+            document.querySelector("#btn_selected_a").style.visibility = "visible";
+            document.querySelector("#btn_a").style.opacity = "0";
+        }
+
+        function hideSelectedA() {
+            document.querySelector("#btn_selected_a").style.visibility = "hidden";
+            document.querySelector("#btn_a").style.opacity = "1";
+        }
+
+        //ss btn changed
+        function showSelectedSS() {
+            document.querySelector("#btn_selected_ss").style.visibility = "visible";
+            document.querySelector("#btn_ss").style.opacity = "0";
+        }
+
+        function hideSelectedSS() {
+            document.querySelector("#btn_selected_ss").style.visibility = "hidden";
+            document.querySelector("#btn_ss").style.opacity = "1";
+        }
+
+        //change iso
+        function showSelectedISO() {
+            document.querySelector("#btn_selected_iso").style.visibility = "visible";
+            document.querySelector("#btn_iso").style.opacity = "0";
+        }
+
+        function hideSelectedISO() {
+            document.querySelector("#btn_selected_iso").style.visibility = "hidden";
+            document.querySelector("#btn_iso").style.opacity = "1";
+        }
